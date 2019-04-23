b091bcdf66817c1ef4893efb875b2a94f939328c
richard-graham <richardcolingraham@gmail.com>
2019-04-23 17:50:42 +1200

    added react-router-dom

--- component-mockups/src/App.js
+++ component-mockups/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Router from 'react-router-dom'
 
 import Navigation from './components/navbar/Navigation'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Navigation />
+
       </div>
     );
   }
