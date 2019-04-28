242b6683b7cad0316edc8e544f9e21e61bf09aa2
richard-graham <richardcolingraham@gmail.com>
2019-04-29 10:46:23 +1200

    brought in redux user reducers and actions

--- the-gear-report-client/src/redux/reducers/userReducer.js
+++ the-gear-report-client/src/redux/reducers/userReducer.js
@@ -1,6 +1,13 @@
 import { 
   SET_USER_LOCATION,
-  SET_USER_COUNTRY
+  SET_USER_COUNTRY,
+  SET_USER, 
+  SET_AUTHENTICATED, 
+  SET_UNAUTHENTICATED,
+  LOADING_USER,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  MARK_NOTIFICATIONS_READ
 } from '../types'
 
 const initialState = {
@@ -28,6 +35,45 @@ export default function(state = initialState, action){
         ...state,
         ...action.payload 
       }
+      case SET_AUTHENTICATED:
+      return {
+        ...state,
+        authenticated: true
+      }
+    case SET_UNAUTHENTICATED:
+      return initialState
+    case SET_USER:
+      return {
+        authenticated: true,
+        loading: false,
+        ...action.payload
+      }
+    case LOADING_USER:
+      return {
+        ...state,
+        loading: true
+      }
+    case LIKE_SCREAM:
+      return {
+        ...state,
+        likes: [
+          ...state.likes,
+          {
+            userHandle: state.credentials.handle,
+            screamId: action.payload.screamId
+          }
+        ]
+      }
+    case UNLIKE_SCREAM:
+      return {
+        ...state,
+        likes: state.likes.filter((like) => like.screamId !== action.payload.screamId)
+      }
+    case MARK_NOTIFICATIONS_READ:
+     state.notifications.forEach(note => note.read = true)
+     return {
+       ...state
+     }
     default:
       return state
   }
--- the-gear-report-client/src/redux/types.js
+++ the-gear-report-client/src/redux/types.js
@@ -1,6 +1,13 @@
  // User reducers
- export const SET_USER_LOCATION = 'SET_USER_LOCATION'
- export const SET_USER_COUNTRY = 'SET_USER_COUNTRY'
+export const SET_USER_LOCATION = 'SET_USER_LOCATION'
+export const SET_USER_COUNTRY = 'SET_USER_COUNTRY'
+export const SET_USER = 'SET_USER'
+export const SET_ERRORS = 'SET_ERRORS'
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
+export const LOADING_UI = 'LOADING_UI'
+export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED'
+export const LOADING_USER = 'LOADING_USER'
+export const MARK_NOTIFICATIONS_READ = 'MARK_NOTIFICATIONS_READ'
  // UI reducers
  // Data reducers
 export const LOADING_DATA = 'LOADING_DATA'
