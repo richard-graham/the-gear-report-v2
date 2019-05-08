427acd1a5871744dcb43336cffecc5e918d8dfa9
richard-graham <richardcolingraham@gmail.com>
2019-05-08 13:10:33 +1200

    recent alerts displaying on table.

---
+++ the-gear-report-client/src/components/pages/home/RecentAlerts.js
@@ -0,0 +1,213 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { getAlerts } from '../../../redux/actions/dataActions'
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { AutoSizer, Column, SortDirection, Table } from 'react-virtualized';
+//Mui
+import { withStyles } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography'
+
+const styles = theme => ({
+  table: {
+    fontFamily: theme.typography.fontFamily,
+  },
+  flexContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    boxSizing: 'border-box',
+  },
+  tableRow: {
+    cursor: 'pointer',
+  },
+  tableRowHover: {
+    '&:hover': {
+      backgroundColor: theme.palette.grey[200],
+    },
+  },
+  tableCell: {
+    flex: 1,
+  },
+  noClick: {
+    cursor: 'initial',
+  },
+  shaded: {
+    backgroundColor: 'lightBlue'
+  }
+});
+
+class ReactVirtualizedTable extends React.PureComponent {
+  getRowClassName = ({ index }) => {
+    const { classes, rowClassName, onRowClick } = this.props;
+    console.log(index);
+
+    return classNames(classes.tableRow, classes.flexContainer, rowClassName, {
+      [classes.tableRowHover]: index !== -1 && onRowClick != null,
+    },
+    index % 2 && index !== -1 ? classes.shaded : '');
+  };
+
+  cellRenderer = ({ cellData, columnIndex = null }) => {
+    const { columns, classes, rowHeight, onRowClick } = this.props;
+    return (
+      <TableCell
+        component="div"
+        className={classNames(classes.tableCell, classes.flexContainer, {
+          [classes.noClick]: onRowClick == null,
+        })}
+        variant="body"
+        style={{ height: rowHeight }}
+        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
+      >
+        {cellData}
+      </TableCell>
+    );
+  };
+
+  headerRenderer = ({ label, columnIndex, dataKey, sortBy, sortDirection }) => {
+    const { headerHeight, columns, classes, sort } = this.props;
+    const direction = {
+      [SortDirection.ASC]: 'asc',
+      [SortDirection.DESC]: 'desc',
+    };
+
+    const inner =
+      !columns[columnIndex].disableSort && sort != null ? (
+        <TableSortLabel active={dataKey === sortBy} direction={direction[sortDirection]}>
+          {label}
+        </TableSortLabel>
+      ) : (
+        label
+      );
+
+    return (
+      <TableCell
+        component="div"
+        className={classNames(classes.tableCell, classes.flexContainer, classes.noClick)}
+        variant="head"
+        style={{ height: headerHeight }}
+        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
+      >
+        {inner}
+      </TableCell>
+    );
+  };
+
+  render() {
+    const { classes, columns, ...tableProps } = this.props;
+    return (
+      <AutoSizer>
+        {({ height, width }) => (
+          <Table
+            className={classes.table}
+            height={height}
+            width={width}
+            {...tableProps}
+            rowClassName={this.getRowClassName}
+          >
+            {columns.map(({ cellContentRenderer = null, className, dataKey, ...other }, index) => {
+              let renderer;
+              if (cellContentRenderer != null) {
+                renderer = cellRendererProps =>
+                  this.cellRenderer({
+                    cellData: cellContentRenderer(cellRendererProps),
+                    columnIndex: index,
+                  });
+              } else {
+                renderer = this.cellRenderer;
+              }
+
+              return (
+                <Column
+                  key={dataKey}
+                  headerRenderer={headerProps =>
+                    this.headerRenderer({
+                      ...headerProps,
+                      columnIndex: index,
+                    })
+                  }
+                  className={classNames(classes.flexContainer, className)}
+                  cellRenderer={renderer}
+                  dataKey={dataKey}
+                  {...other}
+                />
+              );
+            })}
+          </Table>
+        )}
+      </AutoSizer>
+    );
+  }
+}
+
+ReactVirtualizedTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      cellContentRenderer: PropTypes.func,
+      dataKey: PropTypes.string.isRequired,
+      width: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  headerHeight: PropTypes.number,
+  onRowClick: PropTypes.func,
+  rowClassName: PropTypes.string,
+  rowHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
+  sort: PropTypes.func,
+};
+
+ReactVirtualizedTable.defaultProps = {
+  headerHeight: 56,
+  rowHeight: 56,
+};
+
+const WrappedVirtualizedTable = withStyles(styles)(ReactVirtualizedTable);
+
+class RecentAlerts extends Component {
+
+  componentDidMount = () => {
+    this.props.getAlerts()
+  }
+
+  render() {
+    const { alerts } = this.props
+    return (
+      <Paper style={{ height: 300, width: '100%' }}>
+      <Typography variant='subtitle1'>Recent Tickets</Typography>
+        <WrappedVirtualizedTable
+          rowCount={alerts.length > 0 ? alerts.length : ''}
+          rowGetter={({ index }) => alerts[index]}
+          onRowClick={event => console.log(event)}
+          columns={[
+            {
+              width: 200,
+              flexGrow: 1.0,
+              label: 'Title',
+              dataKey: 'title',
+            },
+            {
+              width: 120,
+              label: 'Location',
+              dataKey: 'location',
+            },
+            {
+              width: 120,
+              label: 'Severity',
+              dataKey: 'severity',
+              numeric: true,
+            }
+          ]}
+        /> 
+      </Paper>
+    ) 
+  }
+}
+
+const mapStateToProps = state => ({
+  alerts: state.data.alerts
+})
+
+export default connect(mapStateToProps, { getAlerts })(RecentAlerts)
