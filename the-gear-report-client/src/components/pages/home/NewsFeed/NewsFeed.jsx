1422ae6934de43b42e107610a331c46dbebd912f
richard-graham <richardcolingraham@gmail.com>
2019-12-04 09:03:41 +1300

    Running into server errors. Need to do some tidying up before I carry on developing. Most seem to be Stripe CORS related errors

--- the-gear-report-client/src/components/pages/home/NewsFeed/NewsFeed.jsx
+++ the-gear-report-client/src/components/pages/home/NewsFeed/NewsFeed.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import moment from 'moment'
 import { getFirstNewsFeed, getAdditionalNewsFeed } from '../../../../redux/actions/newsFeedActions'
+import NewAlert from './components/NewAlert'
+import NewComment from './components/NewComment'
+import NewWorkPlan  from './components/NewWorkPlan'
+import WorkPlanStatus  from './components/WorkPlanStatus'
 //Mui
 import withStyles from '@material-ui/core/styles/withStyles'
 
@@ -13,24 +15,46 @@ export class NewsFeed extends Component {
     this.props.getFirstNewsFeed()
   }
 
+  feedItem = (item) => {
+    switch(item.type){
+      case 'newAlert':
+        return <NewAlert props={item} />
+      case 'newComment':
+        return <NewComment props={item} />
+      case 'newWorkPlan':
+        return <NewWorkPlan props={item} />
+      case 'completedWorkPlan':
+        return <WorkPlanStatus props={item} />
+      case 'unCompletedWorkPlan':
+        return <WorkPlanStatus props={item} />
+    }
+  }
+
   render() {
 
-    const { classes } = this.props
+    const { classes, newsFeed, loadingNewsFeed } = this.props
 
     return (
       <div className={classes.newsFeedContainer}>
-        
+        {newsFeed.map(feedBlock => {
+          console.log(feedBlock);
+          feedBlock.forEach(item => this.feedItem(item))
+          // If loading spin stuff
+        })}
       </div>
     )
   }
 }
 
 const styles = {
- 
+  newsFeedContainer: {
+
+  }
 }
 
 const mapStateToProps = state => ({
-
+  newsFeed: state.data.newsFeed.items,
+  loadingNewsFeed: state.data.newsFeed.loading
 })
 
 const mapDispatchToProps = {
